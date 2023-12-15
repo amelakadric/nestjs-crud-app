fbc94bc6c898674f6fe9331c986229ecfa50e42d
Amela Kadrić <amela.kadric@interns.rbt.rs>
2023-12-15 11:11:14 +0100

    added validation

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,12 @@
-export class CreateUserDto { 
-    email:string;
-    name: string;
-    type: number;
+import { IsEmail, IsInt, IsString } from 'class-validator';
 
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  name: string;
+
+  @IsInt()
+  type: number;
 }
